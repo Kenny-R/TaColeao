9c758337321754a6d610c8bb3a7295d901e78a48
Ana <73360276+anna-ayn@users.noreply.github.com>
2023-03-09 19:58:10 -0400

    dos funciones mas

--- utilidades.h
+++ utilidades.h
@@ -6,4 +6,23 @@
 
     int comprobarEntrada(int argc, char *argv[], char *nameC, double *t);
     time_t strToTime(char *str);
+
+    /*
+    Funcion que cuenta el numero de rutas mediante un archivo
+        Argumentos:
+        *fp: un apuntador a un archivo
+    Retorna:
+    n: numero de rutas
+    */
+    int get_number_routes(FILE *fp);
+
+    /*
+    Funcion que lee el archivo de carga y construye el arreglo donde se guarda estas cargas
+    Argumentos:
+        n: un entero que indica el numero de rutas
+        *fp: un apuntador a un archivo
+        arr: un arreglo de tipo t_carga
+    */
+    void build_loads(int n, FILE *fp, t_carga *arr[]);
+
 #endif
