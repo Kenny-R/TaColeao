a3c4199232d5acd472f8e73d34552aa37d4e1026
Ana <73360276+anna-ayn@users.noreply.github.com>
2023-03-18 20:11:05 -0400

    codificarInformacion

--- utilidades.c
+++ utilidades.c
@@ -5,7 +5,7 @@
 #include <sys/wait.h>
 #include "utilidades.h"
 
-/* 
+/*
     Comprueba los datos de entrada si todo fue correcto un numero distinto mayor a 0 si algo fallo devuelve un numero menor
     o igual a 0
     Argumentos:
@@ -13,77 +13,92 @@
         argv: arreglo de argumentos
         nameC: string donde se almacenara el nombre del archivo de carga
         nameS: string donde se almacenara el nombre del archivo de servicio
-        t: Valor real donde se almacenara la correspondecia de un minuto de 
+        t: Valor real donde se almacenara la correspondecia de un minuto de
            simulacion en segundos reales
     Retrona:
-        Un numero mayor que 0 si todo fue bien. Un numero menor o igual a 0 si algo salio mal 
+        Un numero mayor que 0 si todo fue bien. Un numero menor o igual a 0 si algo salio mal
 */
-int comprobarEntrada(int argc, char *argv[], char *nameC, char *nameS, double *t) {
-    FILE * temp;
+int comprobarEntrada(int argc, char *argv[], char *nameC, char *nameS, double *t)
+{
+    FILE *temp;
     /*comprobamos que la entrada tenga mas de 1 argumento*/
-    if (argc < 3) {
-        printf("Error, too few arguments\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n",argv[0]);
+    if (argc < 3)
+    {
+        printf("Error, too few arguments\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[0]);
         return -1;
     }
 
     /* comprobamos que el -s es correcto */
-     if (strcmp(argv[1],"-s") != 0){
-            printf("Error with the arguments\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[0]);
-            return -1;
+    if (strcmp(argv[1], "-s") != 0)
+    {
+        printf("Error with the arguments\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[0]);
+        return -1;
     }
 
-    if ((temp = fopen(argv[2], "r")) == NULL){
+    if ((temp = fopen(argv[2], "r")) == NULL)
+    {
         printf("Error with the argument -s \"%s\"\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[2], argv[0]);
         return -1;
     }
     fclose(temp);
-    memset(nameS,0,MAX_NAME_FILE);
-    strcpy(nameS,argv[2]);
+    memset(nameS, 0, MAX_NAME_FILE);
+    strcpy(nameS, argv[2]);
 
-    if (argc == 5) {
-        if (strcmp(argv[3],"-c") == 0) { /* si existe comprobamos que el -c es correcto*/
-            
-            if ((temp = fopen(argv[4], "r")) == NULL){
+    if (argc == 5)
+    {
+        if (strcmp(argv[3], "-c") == 0)
+        { /* si existe comprobamos que el -c es correcto*/
+
+            if ((temp = fopen(argv[4], "r")) == NULL)
+            {
                 printf("Error with the argument -c \"%s\"\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[4], argv[0]);
                 return -1;
             }
             fclose(temp);
-            memset(nameC,0,MAX_NAME_FILE);
-            strcpy(nameC,argv[4]);
-
-        } else if (strcmp(argv[3],"-t") == 0) { /* si existe comprobamos que el -t es correcto*/
-            if (atof(argv[4]) <= 0){ /* no puedes tener un tiempo de 0 o negativo*/
-                printf("Error with the argument -t \"%s\"\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n",argv[4],argv[0]);
+            memset(nameC, 0, MAX_NAME_FILE);
+            strcpy(nameC, argv[4]);
+        }
+        else if (strcmp(argv[3], "-t") == 0)
+        { /* si existe comprobamos que el -t es correcto*/
+            if (atof(argv[4]) <= 0)
+            { /* no puedes tener un tiempo de 0 o negativo*/
+                printf("Error with the argument -t \"%s\"\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[4], argv[0]);
                 return -1;
             }
 
             *t = atof(argv[4]);
 
-            if ((temp = fopen(nameC, "r")) == NULL){
+            if ((temp = fopen(nameC, "r")) == NULL)
+            {
                 printf("Error with the file \"%s\" maybe it doesn't exists \n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", nameC, argv[0]);
                 return -1;
             }
             fclose(temp);
-
-        } else {
+        }
+        else
+        {
             printf("Error with the arguments\n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[0]);
             return -1;
         }
-    } else if (argc == 7) {
-        if ((strcmp(argv[3],"-c") != 0) || ((temp = fopen(argv[4], "r")) == NULL)){
-                printf("Error with the argument -c \n Use: %s -s <file name> [-c <file name>] [-t <num>]\n",argv[0]);
-                return -1;
-            }
+    }
+    else if (argc == 7)
+    {
+        if ((strcmp(argv[3], "-c") != 0) || ((temp = fopen(argv[4], "r")) == NULL))
+        {
+            printf("Error with the argument -c \n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[0]);
+            return -1;
+        }
         fclose(temp);
-        
-        memset(nameC,0,MAX_NAME_FILE);
-        strcpy(nameC,argv[4]);
-        
-        if ((strcmp(argv[5],"-t") != 0) || (atof(argv[6]) <= 0)){ /* no puedes tener un tiempo de 0 o negativo*/
-                printf("Error with the argument -t \n Use: %s -s <file name> [-c <file name>] [-t <num>]\n",argv[0]);
-                return -1;
-            }
-            
+
+        memset(nameC, 0, MAX_NAME_FILE);
+        strcpy(nameC, argv[4]);
+
+        if ((strcmp(argv[5], "-t") != 0) || (atof(argv[6]) <= 0))
+        { /* no puedes tener un tiempo de 0 o negativo*/
+            printf("Error with the argument -t \n Use: %s -s <file name> [-c <file name>] [-t <num>]\n", argv[0]);
+            return -1;
+        }
+
         *t = atof(argv[6]);
     }
 
@@ -96,15 +111,15 @@ int comprobarEntrada(int argc, char *argv[], char *nameC, char *nameS, double *t
         horas: un entero que representa las horas
         minutos: un entero que representa los minutos
     Retorna:
-        Un numero mayor que 0 si todo fue bien. Un numero menor o igual a 0 si algo salio mal 
+        Un numero mayor que 0 si todo fue bien. Un numero menor o igual a 0 si algo salio mal
 */
-int verificarTiempo(int horas, int minutos) {
-    if (horas > 23 || horas < 0) 
+int verificarTiempo(int horas, int minutos)
+{
+    if (horas > 23 || horas < 0)
         return 0;
     if (minutos > 59 || minutos < 0)
         return 0;
     return 1;
-    
 }
 
 /*
@@ -177,8 +192,8 @@ void build_loads(int n, FILE *fp, t_carga *arr[])
     int g[8];
     int i;
     for (i = 0; i < n; ++i)
-    {        
-        fscanf(fp, "%[^,], %[^,], %[^,], %d, %d, %d, %d, %d, %d, %d, %d", code, load_name,travel_time, &g[0], &g[1], &g[2], &g[3], &g[4], &g[5], &g[6], &g[7]);
+    {
+        fscanf(fp, "%[^,], %[^,], %[^,], %d, %d, %d, %d, %d, %d, %d, %d", code, load_name, travel_time, &g[0], &g[1], &g[2], &g[3], &g[4], &g[5], &g[6], &g[7]);
 
         /* Creamos la carga con los datos que nos dieron */
         new_load = crearCarga(code, load_name, travel_time);
@@ -193,16 +208,15 @@ void build_loads(int n, FILE *fp, t_carga *arr[])
         agregarGrupo("12:00", g[6], new_load);
         agregarGrupo("13:00", g[7], new_load);
 
-
         /* Agregamos esta nueva carga al arreglo */
         arr[i] = new_load;
 
         /* obtenemos el caracter de la nueva linea */
-        if(getc(fp) != '\n'){
+        if (getc(fp) != '\n')
+        {
             getc(fp);
         }
     }
-
 }
 
 /*
@@ -218,7 +232,7 @@ void build_services(int n, FILE *fp, itinerario *arr[])
     itinerario *new_itinerary;
     char c = getc(fp);
     while (c != EOF)
-    {   
+    {
         if (isalpha(c))
         {
             /* Leemos el codigo de la parada */
@@ -234,14 +248,14 @@ void build_services(int n, FILE *fp, itinerario *arr[])
         {
             /* Leemos la hora en que sale el autobus y su capacidad */
             /*(Tiene el mismo tamaño que un travel time)*/
-            char *string_hora = malloc(MAX_TRAVEL_TIME_LENGTH);
+            char *string_hora = calloc(MAX_TRAVEL_TIME_LENGTH, sizeof(char));
             while (isdigit(c) || c == ':')
             {
                 strncat(string_hora, &c, 1);
                 c = getc(fp);
             }
             fscanf(fp, "%d", &capacity);
-            agregarServicio(string_hora,capacity,new_itinerary);
+            agregarServicio(string_hora, capacity, new_itinerary);
         }
         else if (c == '\n')
         {
@@ -256,15 +270,16 @@ void build_services(int n, FILE *fp, itinerario *arr[])
 }
 
 /*
-    Dado el nombre de un archivo que representa una representacion de la carga de una parada, 
+    Dado el nombre de un archivo que representa una representacion de la carga de una parada,
     rellena un array con todos los datos del archivo.
     Argumento:
         carga: El nombre(o direccion) del archivo a leer
         numeroRutas: un apuntador a un entero donde se almacenara el numero de rutas del archivo
     Retorna:
-        Un arreglo de structs t_carga que representa la carga de cada parada 
+        Un arreglo de structs t_carga que representa la carga de cada parada
 */
-t_carga ** leerCarga(char *carga, int *numeroRutas)  {
+t_carga **leerCarga(char *carga, int *numeroRutas)
+{
     /* numero de rutas */
     int n = 0;
 
@@ -279,12 +294,12 @@ t_carga ** leerCarga(char *carga, int *numeroRutas)  {
         return NULL;
     }
     n = get_number_routes(load_file);
-    
+
     *numeroRutas = n;
-    
+
     /* Creamos un arreglo para las cargas */
-    t_carga **loads=malloc(sizeof(t_carga*)*n);
-    
+    t_carga **loads = malloc(sizeof(t_carga *) * n);
+
     /* Volvemos a leer desde el comienzo del archivo */
     rewind(load_file);
     /* Y construiremos el arreglo de las cargas */
@@ -296,15 +311,16 @@ t_carga ** leerCarga(char *carga, int *numeroRutas)  {
 }
 
 /*
-    Dado el nombre de un archivo que representa una representacion del servicio de una parada, 
+    Dado el nombre de un archivo que representa una representacion del servicio de una parada,
     rellena un array con todos los datos del archivo.
     Argumento:
         servicio: El nombre(o direccion) del archivo a leer
         numeroRutas: un apuntador a un entero donde se almacenara el numero de rutas del archivo
     Retorna:
-        Un arreglo de structs itinerario que representa el servicio para cada parada 
+        Un arreglo de structs itinerario que representa el servicio para cada parada
 */
-itinerario ** leerServicio(char *servicio, int *numeroRutas)  {
+itinerario **leerServicio(char *servicio, int *numeroRutas)
+{
     /* numero de rutas */
     int n = 0;
 
@@ -320,32 +336,85 @@ itinerario ** leerServicio(char *servicio, int *numeroRutas)  {
         printf("The file could not be open\n");
         return NULL;
     }
-    
+
     n = get_number_routes(service_file) + 1;
     *numeroRutas = n;
-    
+
     rewind(service_file);
     /* Creamos un arreglo para guardar los servicios de cada parada */
-    itinerario **routes_service = malloc(sizeof(itinerario*)*n);
+    itinerario **routes_service = malloc(sizeof(itinerario *) * n);
     build_services(n, service_file, routes_service);
-    
+
     /* Cerramos el archivo */
     fclose(service_file);
     return routes_service;
 }
 
 /*
-    Dado un apuntador a un time_t imprime la informacion en 
+    Dado un apuntador a un time_t imprime la informacion en
     formato HH:MM. Pone un \n al final de la impresion siempre
     Argumento:
         hora: el time_t con la infromacion
 */
-void imprimirHora(time_t *hora) {
+void imprimirHora(time_t *hora)
+{
     char time[MAX_TRAVEL_TIME_LENGTH];
-    strftime(time,MAX_TRAVEL_TIME_LENGTH,"%H:%M",localtime(hora));
-    printf("%s\n",time);
+    strftime(time, MAX_TRAVEL_TIME_LENGTH, "%H:%M", localtime(hora));
+    printf("%s\n", time);
 }
 
-double segToSmin(double t) {
+double segToSmin(double t)
+{
     return (1 / t);
+}
+
+int numeroDePersonasEnEspera(t_carga *infoCarga, time_t hora)
+{
+    nodo *nodoActual = infoCarga->grupos->siguiente;
+    t_grupo *contenido = (t_grupo *)(nodoActual->contenido);
+    int nro = 0;
+    while (contenido != NULL)
+    {
+        if (difftime(hora, contenido->hora) >= 0)
+        {
+            nro = nro + contenido->cantidad;
+            nodoActual = nodoActual->siguiente;
+            contenido = (t_grupo *)(nodoActual->contenido);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return nro;
+}
+
+char *codficarInformacion(struct avance *arrAvances, char *nombreRuta, int nroPersonasEnEspera, int serviciosArrancados, int largoTotal)
+{
+    char *resultado = calloc(largoTotal, sizeof(char));
+    strcat(resultado, "I,");
+    char buffer[sizeof(int) * 8 + 1];
+    sprintf(buffer, "%d", nroPersonasEnEspera);
+    strcat(resultado, buffer);
+    int i = 0;
+    for (; i < serviciosArrancados; i++)
+    {
+        if (arrAvances[i].ida != 2)
+        {
+            strcat(resultado, ",");
+            if (arrAvances[i].ida == 1)
+            {
+                strcat(resultado, ">");
+            }
+            else
+            {
+                strcat(resultado, "<");
+            }
+            sprintf(buffer, "%d", (arrAvances[i].arrPorcentajes[arrAvances[i].pos]) / 10);
+            strcat(resultado, buffer);
+        }
+    }
+    strcat(resultado, "\n");
+    return resultado;
 }
