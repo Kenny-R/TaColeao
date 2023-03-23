e866386748a6c5957f122cc0da07928482d90302
Ana <73360276+anna-ayn@users.noreply.github.com>
2023-03-23 09:52:56 -0400

    documentacion de funciones

--- utilidades.h
+++ utilidades.h
@@ -19,7 +19,7 @@
         int ida; /* 1 indica que el autobus va de la uni->parada, 0 si indica que va de parada->uni, -1 indica que esta subiendo los pasajeros y 2 si ya termino */
         int pos; /* posicion del servicio dentro del arreglo porcentajes  */
         time_t tiempoRecorr; /* tiempo de recorrido del autobus */ 
-        time_t *horaActual; /* hora actual de la simulacion */
+        time_t *horaActual; /* un apuntador a la hora actual de la simulacion */
         time_t horaPartida; /* hora en que parte el autobus hacia la parada */
         int *go; /* un arreglo que indica si es el momento en que el hilo actualice su estado */
         int *servicios_terminados; /* numero de servicios terminados */
@@ -107,25 +107,64 @@
     */
     void imprimirHora(time_t *hora);
 
-    double segToSmin(double t);
+    /*
+        Envia un mensaje a traves de un pipe
+        Argumentos:
+            fd: file descriptor para el pipe de escritura
+            origen: un string que indica quien es el que escribe el mensaje
+            destino: un string que indica a quien se le va a enviar el mensaje
+            *hora: un apuntador a la hora en que se envia el mensaje
+            mensaje: el texto que se va a enviar por el pipe
+    */
+    void enviarMensaje(int *fd, char origen[], char destino[], time_t *hora, char mensaje[]);
 
+    /*
+        Lee un mensaje a traves de un pipe
+        Argumentos:
+            *fd: file descriptor para el pipe de lectura
+            origen: un string que indica quien es el que escribió el mensaje
+            destino: un string que indica para quien fue el mensaje
+            *hora: un apuntador a la hora en que se envió el mensaje
+            mensaje: el texto que se envió por el pipe
+    */
+    void leerMensaje(int *fd, char origen[], char destino[], time_t *hora, char mensaje[]);
+
+    /*
+        Dado un struct t_carga y la hora en time_t, va a calcular la cantidad de personas esperando en una ruta
+        Argumento:
+            *infoCarga: un apuntador a la carga de una ruta
+            hora: hora actual de la simulacion
+        Retorna:
+            el numero de personas esperando en la ruta
+    */
     int numeroDePersonasEnEspera(t_carga *infoCarga, time_t hora);
 
+    /*
+        Codifica el mensaje que se va a enviar a traves del pipe a un formato:
+        "I,nP,signo_1.avance_1,...,signo_n.avance_n\n" donde 
+        nP es el número de personas esperando en una parada,
+        signo_i puede ser < o > dependiendo de la dirección del autobús y 
+        avance_i es el porcentaje de avance (del 0% al 100%).
+        Argumento:
+            *arrAvances: un apuntador a un arreglo de struct avance
+            *nombreRuta: un string que contiene el nombre de la ruta
+            nroPersonasEnEspera: el numero de personas esperando en la ruta
+            serviciosArrancados: cantidad de autobuses que se encuentra activos
+            largoTotal: largo del mensaje
+        Retorna:
+            el mensaje codificado
+    */
     char *codficarInformacion(struct avance *arrAvances, char *nombreRuta, int nroPersonasEnEspera, int serviciosArrancados, int largoTotal);
 
     /*
-        Función que decodifica e imprime dado un mensaje de 
-        actualización de estado codificado dado el formato 
+        Función que decodifica e imprime dado un mensaje de actualización de estado codificado dado el formato:
         "I,nP,signo_1.avance_1,...,signo_n.avance_n\n" donde 
         nP es el número de personas esperando en una parada,
-        signo_i puede ser < o > dependiendo de la dirección 
-        del autobús y avance_i es el porcentaje de avance 
-        (del 0% al 100%).
+        signo_i puede ser < o > dependiendo de la dirección del autobús y 
+        avance_i es el porcentaje de avance (del 0% al 100%).
         Argumentos:
-            codParada: arreglo de caracteres que contiene el 
-            código de la parada.
-            msg: arreglo de caracteres que contiene el mensaje
-            de actualización de estado codificado.
+            codParada: arreglo de caracteres que contiene el código de la parada.
+            msg: arreglo de caracteres que contiene el mensaje de actualización de estado codificado.
     */
     void imprimirMsg(char codParada[], char msg[]);
 
